95a4209354a40bf9d817537fab1827f9eae2159b
wheeziequark <ndierking@gmx.de>
2022-11-30 00:49:09 +0100

    Update and rename runtime_4k.sh to runtime_video_4k_200.sh

--- arm/pinephone/runtime_4k.sh
+++
@@ -1,8 +0,0 @@
-while true
-do
-    echo $(date) >> ./battery_4k.txt
-    echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./battery_4k.txt
-    echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./battery_4k.txt
-    echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
-    sleep 60
-done
---
+++ arm/pinephone/runtime_video_4k_200.sh
@@ -0,0 +1,8 @@
+while true
+do
+    echo $(date) >> ./video_4k_200.txt
+    echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./video_4k_200.txt
+    echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./video_4k_200.txt
+    echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
+    sleep 60
+done
