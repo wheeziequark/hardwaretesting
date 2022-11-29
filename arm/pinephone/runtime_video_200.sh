ff6c3abdcca1d87598e4a788e68b3315efa155a3
wheeziequark <ndierking@gmx.de>
2022-11-30 00:46:21 +0100

    Create runtime_video_200.sh

---
+++ arm/pinephone/runtime_video_200.sh
@@ -0,0 +1,8 @@
+while true
+do
+    echo $(date) >> ./battery_video_200.txt
+    echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./battery_video_200.txt
+    echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./battery_video_200.txt
+    echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
+    sleep 60
+done
