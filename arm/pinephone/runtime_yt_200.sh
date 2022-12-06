3535f56ebfb11f548b316a597481b0667cc44e21
wheeziequark <ndierking@gmx.de>
2022-12-06 12:24:44 +0100

    Update runtime_yt_200.sh

--- arm/pinephone/runtime_yt_200.sh
+++ arm/pinephone/runtime_yt_200.sh
@@ -4,5 +4,5 @@ do
     echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./battery_yt_200.txt
     echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./batter_yt_200.txt
     echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
-    sleep 60
+    sleep 300
 done
