28ee23cba0def6998fb6ee00df3212a08ff22d4b
wheeziequark <ndierking@gmx.de>
2022-12-06 12:24:00 +0100

    Update runtime_idle_200.sh

--- arm/pinephone/runtime_idle_200.sh
+++ arm/pinephone/runtime_idle_200.sh
@@ -4,5 +4,5 @@ do
     echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./battery_idle_200.txt
     echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./battery_idle_200.txt
     echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
-    sleep 60
+    sleep 300
 done
