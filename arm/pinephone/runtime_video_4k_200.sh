de91661d335a1e8377cda3e65b11fb36a4b0cc9b
wheeziequark <ndierking@gmx.de>
2022-12-06 12:24:28 +0100

    Update runtime_video_4k_200.sh

--- arm/pinephone/runtime_video_4k_200.sh
+++ arm/pinephone/runtime_video_4k_200.sh
@@ -4,5 +4,5 @@ do
     echo $(cat /sys/class/power_supply/rk818-battery/status) >> ./video_4k_200.txt
     echo $(cat /sys/class/power_supply/rk818-battery/capacity) >> ./video_4k_200.txt
     echo "Akku zu $(cat /sys/class/power_supply/rk818-battery/capacity)% geladen"
-    sleep 60
+    sleep 300
 done
